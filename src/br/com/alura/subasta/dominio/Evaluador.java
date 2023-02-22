1ecde96ce42ea494a998a920488ee624e801e878
Milton Figueredo Miles <gratno@gmail.com>
2023-02-22 16:57:48 -0500

    Probando Funcionalidad

---
+++ src/br/com/alura/subasta/dominio/Evaluador.java
@@ -0,0 +1,13 @@
+package br.com.alura.subasta.dominio;
+
+public class Evaluador {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/br/com/alura/subasta/dominio/Oferta.java
@@ -0,0 +1,21 @@
+package br.com.alura.subasta.dominio;
+
+public class Oferta {
+
+	private Usuario usuario;
+	private double valor;
+
+	public Oferta(Usuario usuario, double valor) {
+		this.usuario = usuario;
+		this.valor = valor;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+}
