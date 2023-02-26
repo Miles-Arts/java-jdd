7cc2dccb7fd1179351ea000ee1ae015c4736d61d
Milton Figueredo Miles <gratno@gmail.com>
2023-02-26 15:42:39 -0500

    Testeando Exception

--- src/test/TestEvaluadorExcepcion.java
+++ src/test/TestEvaluadorExcepcion.java
@@ -1,8 +1,33 @@
 package test;
 
+import org.junit.Test;
+
+import br.com.alura.subasta.dominio.CreadorDeSubasta;
+import br.com.alura.subasta.dominio.Evaluador;
+import br.com.alura.subasta.dominio.Subasta;
+
 public class TestEvaluadorExcepcion {
-	
-	
-	
+
+	@Test(expected = RuntimeException.class)
+	public void deberiaRetornarExceptionCuandoNoHayOfertas() {
+
+		Subasta subasta = new CreadorDeSubasta().para("TV").construir();
+
+		
+		Evaluador evaluador = new Evaluador();
+		evaluador.evalua(subasta);
+		
+		
+		//Con RUNTIME EXCEPTION no necesito en Try Catch
+		/*try {
+
+			Evaluador evaluador = new Evaluador();
+			evaluador.evalua(subasta);
+
+		} catch (Exception ex) {
+			System.out.println("Funcionó!");
+		}*/
+
+	}
 
 }
